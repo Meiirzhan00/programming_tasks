ecd2d5426efd7ffa61ae8a383b715c11d80a3910
Meiirzhan00 <54979320+Meiirzhan00@users.noreply.github.com>
2021-01-09 12:23:48 +0600

    Update OOP_Python.py

--- OOP_Python.py
+++ OOP_Python.py
@@ -1,1 +1,18 @@
+class BMI:
+    
+    name=input("What is your name : ")
+    weight=float(input("How much is your weight: "))
+    height=float(input("How much is your height: "))
 
+    def bmi_calculator(self):
+        
+        bmi = BMI.weight / (BMI.height ** 2)
+        
+        print('Body mass index: '+str(bmi))
+        if bmi<25:
+            return BMI.name+' does not have excess weight !'
+        else:
+            return BMI.name+' has excess weight !'
+
+bmi1=BMI()
+print(bmi1.bmi_calculator())
